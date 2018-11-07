b552214edb20a97aba6991bbab6c4bb970d42732
Jens <jens.hejneskog@yh.nackademin.se>
2018-11-07 13:20:18 +0100

    First commit of solution

---
+++ JensBankWebApp.Test/BankAppTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace JensBankWebApp.Test
+{
+    public class BankAppTest
+    {
+        [Fact]
+        public void TestExempel()
+        {
+            Assert.True(true);
+        }
+    }
+}
